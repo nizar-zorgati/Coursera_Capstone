e84b34db1852823556e22e4a46052b5c0656b729
Mohamed Nizar ZORGATI <61159132+nizar-zorgati@users.noreply.github.com>
2020-10-12 00:01:09 +0100

    Add files via upload

---
+++ Accident severity.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### This is a Coursera's Capstone and i will be working on a case study which is to predict the severity of an accident.
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
